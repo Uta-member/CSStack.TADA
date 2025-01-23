e114b2a3ddb1cd59f210a5228501c43b41441827
Uta Sugane <sugane.ns10@gmail.com>
2025-01-23 21:29:45 +0900

    複数例外を扱える例外クラスを作成

---
+++ CSStack.TADA/Exceptions/MutiReasonException.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+namespace CSStack.TADA
+{
+    /// <summary>
+    /// 複数例外を同時に扱うための例外
+    /// </summary>
+    public class MutiReasonException : Exception
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="innerExceptions"></param>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public MutiReasonException(
+            ImmutableList<Exception> innerExceptions,
+            string? message = null,
+            Exception? innerException = null) : base(message, innerException)
+        {
+            InnerExceptions = innerExceptions;
+        }
+
+        /// <summary>
+        /// 同時発生した例外リスト
+        /// </summary>
+        public ImmutableList<Exception> InnerExceptions { get; }
+    }
+}
