400df409c1d166eba8f9779189e330d8979c3e9d
u_sugane <sugane.ns10@gmail.com>
2025-05-20 12:20:45 +0900

    QueryServiceのメソッドの引数にキャンセルトークンを受け取るようにした

--- CSStack.TADA/UseCase/UseCase/QueryService/IQueryService.cs
+++ CSStack.TADA/UseCase/UseCase/QueryService/IQueryService.cs
@@ -1,17 +1,16 @@
 ﻿namespace CSStack.TADA
 {
-    /// <summary>
-    /// クエリサービスインターフェース
-    /// </summary>
-    public interface IQueryService<TReq, TRes>
-        where TReq : IQueryServiceDTO
-        where TRes : IQueryServiceDTO
-    {
-        /// <summary>
-        /// クエリサービスのメソッドを実行する
-        /// </summary>
-        /// <param name="req">リクエスト</param>
-        /// <returns>戻り値</returns>
-        ValueTask<TRes> ExecuteAsync(TReq req);
-    }
+	/// <summary>
+	/// クエリサービスインターフェース
+	/// </summary>
+	public interface IQueryService<TReq, TRes> where TReq : IQueryServiceDTO where TRes : IQueryServiceDTO
+	{
+		/// <summary>
+		/// クエリサービスのメソッドを実行する
+		/// </summary>
+		/// <param name="req">リクエスト</param>
+		/// <param name="cancellationToken">キャンセルトークン</param>
+		/// <returns>戻り値</returns>
+		ValueTask<TRes> ExecuteAsync(TReq req, CancellationToken cancellationToken = default);
+	}
 }
--- CSStack.TADA/UseCase/UseCase/QueryService/IQueryServiceWithoutReq.cs
+++ CSStack.TADA/UseCase/UseCase/QueryService/IQueryServiceWithoutReq.cs
@@ -1,15 +1,15 @@
 ﻿namespace CSStack.TADA
 {
-    /// <summary>
-    /// クエリサービスインターフェース
-    /// </summary>
-    public interface IQueryServiceWithoutReq<TRes>
-        where TRes : IQueryServiceDTO
-    {
-        /// <summary>
-        /// クエリサービスのメソッドを実行する
-        /// </summary>
-        /// <returns>戻り値</returns>
-        ValueTask<TRes> ExecuteAsync();
-    }
+	/// <summary>
+	/// クエリサービスインターフェース
+	/// </summary>
+	public interface IQueryServiceWithoutReq<TRes> where TRes : IQueryServiceDTO
+	{
+		/// <summary>
+		/// クエリサービスのメソッドを実行する
+		/// </summary>
+		/// <param name="cancellationToken">キャンセルトークン</param>
+		/// <returns>戻り値</returns>
+		ValueTask<TRes> ExecuteAsync(CancellationToken cancellationToken = default);
+	}
 }
