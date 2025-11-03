7ffe6763ef793bd560523356390c03ebdbe848a8
Uta Sugane <sugane.ns10@gmail.com>
2025-11-03 14:14:17 +0900

    バージョン1.0.4: 削除可能なリポジトリを実装
    プロジェクトファイルのバージョンを1.0.4に更新し、リリースノートを「エンティティを削除可能なリポジトリを実装」に変更しました。
    
    新しいインターフェース`IRepositoryDeletable`を追加しました。このインターフェースは、エンティティを削除する機能を持つリポジトリを定義しており、非同期メソッド`DeleteAsync`を提供します。

---
+++ src/CSStack.TADA/Domain/Repository/IRepositoryDeletable.cs
@@ -0,0 +1,38 @@
+﻿namespace CSStack.TADA
+{
+    /// <summary>
+    /// Defines a repository interface that supports deleting entities by their identifier.
+    /// </summary>
+    /// <remarks>
+    /// This interface extends <see cref="IRepository{TEntity, TEntityIdentifier, TOperateInfo, TSession}"/> and adds
+    /// functionality for deleting entities by their identifier. It is designed for scenarios where entity deletion is a
+    /// supported operation within the repository.
+    /// </remarks>
+    /// <typeparam name="TEntity">The type of the entity managed by the repository.</typeparam>
+    /// <typeparam name="TEntityIdentifier">The type of the unique identifier for the entity.</typeparam>
+    /// <typeparam name="TOperateInfo">The type of the operation metadata used during repository operations.</typeparam>
+    /// <typeparam name="TSession">
+    /// The type of the session or transaction context used for repository operations. Must implement <see
+    /// cref="IDisposable"/>.
+    /// </typeparam>
+    public interface IRepositoryDeletable<TEntity, TEntityIdentifier, TOperateInfo, TSession>
+         : IRepository<TEntity, TEntityIdentifier, TOperateInfo, TSession>
+        where TEntity : IEntity<TEntityIdentifier>
+        where TSession : IDisposable
+        where TEntityIdentifier : notnull
+        where TOperateInfo : notnull
+    {
+        /// <summary>
+        /// Delete the entity.
+        /// </summary>
+        /// <param name="session">Transaction factor</param>
+        /// <param name="entity">Entity to delete</param>
+        /// <param name="operateInfo">Operate info</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        ValueTask DeleteAsync(
+            TSession session,
+            TEntity entity,
+            TOperateInfo operateInfo,
+            CancellationToken cancellationToken = default);
+    }
+}
